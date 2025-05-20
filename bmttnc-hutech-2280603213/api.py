912f816d5cd69f4b6a48df26e6fc70e5d788c321
tien.huu <tien.huu@DESKTOP-BBFBN75>
2025-05-20 15:45:11 +0700

    [add] lab-02 vigenere

--- bmttnc-hutech-2280603213/api.py
+++ bmttnc-hutech-2280603213/api.py
@@ -1,23 +1,23 @@
 ﻿from flask import Flask, jsonify, request
-from cipher.caesar import CaesarCipher  
+from cipher.vigenere import VigenereCipher  
 
 app = Flask(__name__)
-caesar_cipher = CaesarCipher()
+vigenere_cipher = VigenereCipher()
 
-@app.route('/api/caesar/encrypt', methods=["POST"])
-def caesar_encrypt():
+@app.route('/api/vigenere/encrypt', methods=["POST"])
+def vigenere_encrypt():
     data = request.json
     plain_text = data['plain_text']
-    key = int(data['key'])
-    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
     return jsonify({"encrypted_text": encrypted_text})
 
-@app.route('/api/caesar/decrypt', methods=["POST"])
-def caesar_decrypt():
+@app.route('/api/vigenere/decrypt', methods=["POST"])
+def vigenere_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
-    key = int(data['key'])
-    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({"decrypted_text": decrypted_text})
 
 if __name__ == "__main__":
