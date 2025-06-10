ddabd06acf8a0fd320949c6ad83429794d1c06ec
tien.huu <tien.huu@DESKTOP-BBFBN75>
2025-06-10 15:07:26 +0700

    add client/server, hash

---
+++ lab-04/hash/md5_library.py
@@ -0,0 +1,10 @@
+﻿import hashlib
+
+def calculate_md5(input_string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(input_string.encode('utf-8'))
+    return md5_hash.hexdigest()
+
+input_string = input("Nhập chuỗi cần băm: ")
+md5_hash = calculate_md5(input_string)
+print("Mã bảm MD5 của chuỗi '{}' là: {}".format(input_string, md5_hash))
